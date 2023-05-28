8694d91e05447d92f4eefecf5f81df00b37225a6
WatanabeMasahiro <2c.nave.stk@gmail.com>
2023-05-28 17:16:12 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // "./src/**/*.{html,js}",
+    "./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        cyan: '#9cdbff',
+      },
+      // maxWidth: {
+      //   custom: '60rem',
+      // },
+      // spacing:{
+      //   76: '19rem',
+      // },
+      // fontSize: {
+      //   xxs:['0.624em', {lineHeight: '1rem'}],
+      // },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
+
